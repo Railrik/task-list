7f4c2c65c489b8157feb033e108204888e91d8fe
Railrik <benjamin@ligny.pro>
2023-09-06 19:09:40 +0200

    Mise à jour du background-color Task et colum quand je glisse et dépose une tache

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
 margin:8px;
@@ -8,17 +8,21 @@ padding:8px;
 border:1px solid lightgrey;
 border-radius:2px;
 margin-bottom : 8px;
-background-color:white;
+background-color: white;
+&.dragging {
+  background-color: lightgreen;
+}
 `;
 
 const Task = ({ task, index }) => {
     return (
         <Draggable draggableId={task.id} index={index}>
-            {(provided) => (
+            {(provided, snapshot) => (
                 <Container
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
+                    className={snapshot.isDragging ? 'dragging' : ''}
                 >
                     {task.content}
                 </Container>
